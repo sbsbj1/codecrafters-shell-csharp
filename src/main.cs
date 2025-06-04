0726437d1322cf1418c19f1e290b501cbe91e23d
sbsbj1 <lucasacevedo095@gmail.com>
2025-06-03 20:13:11 -0400

    codecrafters submit [skip ci]

--- src/main.cs
+++ src/main.cs
@@ -4,10 +4,15 @@ using System.Net.Sockets;
 
 
 
-bool repl = true;
+
 while (true)
 {
     Console.Write("$ ");
     var command = Console.ReadLine();
+    if( command == "exit 0")
+    {
+        break;
+    }
     Console.WriteLine($"{command}: command not found");
+    
 }
