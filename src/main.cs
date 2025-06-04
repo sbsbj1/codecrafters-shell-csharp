0e9ecee1f8fb6af77873a42dcd3e0b4be3a0cf9d
sbsbj1 <lucasacevedo095@gmail.com>
2025-06-03 20:01:55 -0400

    codecrafters submit [skip ci]

--- src/main.cs
+++ src/main.cs
@@ -2,8 +2,12 @@ using System.Net;
 using System.Net.Sockets;
 
 
-Console.Write("$ ");
 
 
-var command = Console.ReadLine();
-Console.WriteLine($"{command}: command not found");
+bool repl = true;
+while (true)
+{
+    Console.Write("$ ");
+    var command = Console.ReadLine();
+    Console.WriteLine($"{command}: command not found");
+}
