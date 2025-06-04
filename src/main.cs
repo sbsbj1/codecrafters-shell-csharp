34f4d2fdd7d84f982ebe8650683f8b1531aea898
sbsbj1 <lucasacevedo095@gmail.com>
2025-06-03 22:07:34 -0400

    codecrafters submit [skip ci]

--- src/main.cs
+++ src/main.cs
@@ -3,19 +3,30 @@ using System.Net.Sockets;
 
 
 
-
+String[] builtin = { "exit", "echo", "type" };
 while (true)
 {
     Console.Write("$ ");
     var command = Console.ReadLine();
-    if( command == "exit 0")
+    if( command.ToLower() == "exit 0")
     {
         break;
     }
-    if ( command.Contains("echo "))
+    if ( command.ToLower().Contains("echo "))
     {
         var echo = command.Substring(5);
         Console.WriteLine(echo);
+        continue;
+    }
+    if(command.ToLower().Contains("type "))
+    {
+        var subString = command.Substring(5);
+        if (builtin.Any(subString.Contains))
+        {
+            Console.WriteLine($"{subString} is a shell builtin");
+            continue;
+        }
+        Console.WriteLine($"{subString}: not found");
         continue;
     }
     Console.WriteLine($"{command}: command not found");
